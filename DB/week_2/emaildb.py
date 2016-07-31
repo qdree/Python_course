2bff9bcd090c740e9d9adaa79369b76a6864470d
qdree <ta4ckpih@gmail.com>
2016-07-31 23:01:38 +0300

    added support of regex

--- DB/week_2/emaildb.py
+++ DB/week_2/emaildb.py
@@ -1,4 +1,5 @@
 import sqlite3
+import re
 
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
@@ -14,19 +15,20 @@ if ( len(fname) < 1 ) : fname = 'mbox-short.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: ') : continue
-    words = line.split()
-    email = words[1]
-    pieces = email.split('@')
-    org = pieces[1]
-    print org
-    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org, ))
+    # words = line.split()
+    # email = words[1]
+    # pieces = email.split('@')
+    # org = pieces[1]
+    org = re.findall('@(\S*\S)',line)
+    print org[0]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org[0], ))
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (org, count) 
-                VALUES ( ?, 1 )''', ( org, ) )
+                VALUES ( ?, 1 )''', (org[0], ) )
     else : 
         cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?', 
-            (org, ))
+            (org[0], ))
     # This statement commits outstanding changes to disk each 
     # time through the loop - the program can be made faster 
     # by moving the commit so it runs only after the loop completes
