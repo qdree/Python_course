8fd3ef020a4ee2a6a4575e1b2527e5f9bc1a8f3c
qdree <ta4ckpih@gmail.com>
2016-08-28 23:10:35 +0300

    assignment for 5-th week's task of DB course

---
+++ DB/week_5/geodata/where.js
@@ -0,0 +1,16 @@
+myData = [
+[49.839683,24.029717, 'Lviv, Lviv Oblast, Ukraine'],
+[42.340082,-71.0894884, 'Northeastern, Boston, MA 02115, USA'],
+[40.7399972,-74.1775311, 'Bradley Hall, 110 Warren St, Newark, NJ 07102, USA'],
+[32.776653,35.020411, 'Technion/Dormitories, Haifa'],
+[18.4596142,73.8854376, 'Vishwakarma Institute of Information Technology, Survey No. 2/3/4, VIM Private Rd, Kapil Nagar, Kondhwa, Pune, Maharashtra 411048, India'],
+[33.1561058,131.826132, 'Japan, 〒875-0002 Ōita-ken, Usuki-shi, Shitanoe, 1232−2, ＵＭＤ'],
+[42.4036848,-71.120482, 'South Hall Tufts University, 30 Lower Campus Rd, Somerville, MA 02144, USA'],
+[-38.15186,145.1348138, 'Monash University, Frankston VIC 3199, Australia'],
+[53.2948229,69.4047872, 'Kokshetau 020000, Kazakhstan'],
+[40.7127837,-74.0059413, 'New York, NY, USA'],
+[52.2869741,104.3050183, 'Irkutsk, Irkutsk Oblast, Russia'],
+[8.481302,4.611479, 'University Rd, Ilorin, Nigeria'],
+[49.802093,24.0297502, 'Khutorivka St, 40, Lviv, Lviv Oblast, Ukraine'],
+[-25.7688448,28.199104, 'Unisa Observatory Building, Preller St, Pretoria, 0027, South Africa']
+];
